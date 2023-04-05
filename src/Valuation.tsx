9baf16a9580e352b9b60cd3ab9108b8a2e8b7986
Pranchal15 <pranchal.agarwal@gmail.com>
2023-04-05 12:52:48 +0530

    commit log: 1. Styles change in Valuation 2. Added icons

--- src/Valuation.tsx
+++ src/Valuation.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Valuation.css";
-
+import { HiTrendingUp,HiDotsVertical} from "react-icons/hi";
+import {BiCoinStack} from "react-icons/bi"
+import {FaRegComment} from "react-icons/fa"
+import {AiOutlineThunderbolt} from "react-icons/ai"
+import {RiShareForwardLine} from "react-icons/ri"
 
 
 
@@ -13,17 +17,19 @@ export default function Valuation() {
         <img className="profile-pic" src="./profile_pic.jpg" />
         <div className="handle">alfredo</div>
         <div className="timeOfPost"> 6 June</div>
-        <div className="numberOfCoins">2011F</div>
+        
+        <div className="numberOfCoins"><BiCoinStack/><strong>2100</strong></div>
         <div className="grossCoins">Grooss</div>
-        <div className="growthArrow">arrow</div>
-        <div className="options">hey</div>
-
+        {/* <div className="growthArrow">arrow</div> */}
+      <div className="growthArrow"><HiTrendingUp/>
+        </div>  
+        <div className="options"><HiDotsVertical/></div>
       </div>
       <div className="post-image"><img  src='./post_img.png'/></div>
       <div className="likes-row">
-        <div className="thunder">th</div>
-        <div className="comments">com</div>
-        <div className="share">sh</div>
+        <div className="thunder"><AiOutlineThunderbolt/></div>
+        <div className="comments"><FaRegComment/></div>
+        <div className="share"><RiShareForwardLine/></div>
         <div ><button className="lead-button">Lead +100</button></div>
       </div>
       <div className="interested-row">50 Interested</div>
