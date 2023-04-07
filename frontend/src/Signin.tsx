3ba0ab0810bb11551bf0921854c0e3f911f5749d
Pranchal15 <pranchal.agarwal@gmail.com>
2023-04-08 02:19:13 +0530

    commit log: sleep

--- frontend/src/Signin.tsx
+++ frontend/src/Signin.tsx
@@ -1,50 +1,85 @@
 import React, { useEffect, useState } from "react";
 import "./Signin.css";
-
+import { useNavigate } from "react-router-dom";
+import { Post } from "./models/post";
 
 export default function Signin() {
   const [username, setUsername] = useState("");
   const [name, setname] = useState("");
+  const navigate = useNavigate();
 
-  interface UserInput{
-    username:string,
-    name:string
+
+  const [posts, setPosts] = useState();
+   
+  async function loadPosts() {
+  try {
+    const response = await fetch("/api/posts", {
+      method: "GET",
+    });
+    const fin = await response.json();
+    const post = fin[0]
+    setPosts(post);
+
+
+  } catch (error) {
+    console.log(error);
   }
-
-  
-async function createUser(user:UserInput)
-{
-  const response = await fetch("/api/users",
-  {
-    method:"POST",
-    headers:{
-      "Content-Type":"application/json",
-    },
-    body:JSON.stringify(user),
-  });
-  return response.json();
-
 }
 
-useEffect(() => {
-  createUser({"name":"hey","username":"hi"})
+
+  interface UserInput {
+    username: string;
+    name: string;
+  }
+
+  async function createUser(user: UserInput) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    return response.json();
+  }
+
+  async function handleSubmit() {
+    console.log(posts)
+    navigate("/valuation",{state:{posts:posts,currentUsername:username}});
+    let response = await createUser({ name: name, username: username });
 
 
-}, [])
+  }
+  useEffect(() => {loadPosts()}, []);
 
   return (
     <div className="container">
       <p className="title">Enter your details</p>
       <form className="form">
-        <input className="inputbox" type="text" placeholder="Enter your name" />
+        <input
+          className="inputbox"
+          type="text"
+          placeholder="Enter your name"
+          required
+          value={name}
+          onChange={(e) => setname(e.target.value)}
+        />
 
         <input
           className="inputbox"
           type="text"
           placeholder="Enter your username"
+          required
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
-        <input type="submit" className="submit-button" value="Submit"  />
+        <input
+          type="submit"
+          className="submit-button"
+          value="Submit"
+          onClick={() => handleSubmit()}
+        />
       </form>
     </div>
   );
--- frontend/src/Valuation.tsx
+++ frontend/src/Valuation.tsx
@@ -6,45 +6,78 @@ import { FaRegComment } from "react-icons/fa";
 import { AiOutlineThunderbolt } from "react-icons/ai";
 import { RiShareForwardLine } from "react-icons/ri";
 import { Post } from "./models/post";
+import { useLocation } from "react-router-dom";
 
 export default function Valuation() {
   const [posts, setPosts] = useState<Post[]>([]);
+  
+  
+  const location = useLocation();
+    const [editPost,seteditPost] = useState<Post>(location.state.posts);
+  console.log(editPost)
+
+  interface postInput {
+    name?: string;
+    username?: string;
+    grosscoins?: number;
+    netcoins?: number;
+    leadcoins?: number;
+    highestLeader?: string;
+  }
+
+  async function loadPosts() {
+    try {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+      });
+      const posts = await response.json();
+      console.log("jii");
+      setPosts(posts);
+      console.log("hey");
+      console.log(location.state.posts);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+async function handleLead(){
+  const newObj = {...editPost,netcoins:editPost.netcoins+editPost.leadcoins,grosscoins:editPost.grosscoins+editPost.netcoins+editPost.leadcoins,leadcoins:editPost.leadcoins+100,highestLeader:location.state.currentUsername}
+ console.log(newObj)
+  seteditPost(newObj)
+ let response = await handleLeadButton(newObj._id,newObj);
+ console.log(response)    
+
+} 
+  async function handleLeadButton(postId: string, post: postInput) {
+    try {
+      const response = await fetch('/api/posts/'+postId, {
+        method: "PATCH", 
+        headers :{
+          "Content-Type":"application/json",
+        },
+        body:JSON.stringify(post),
+      });
+      return  response.json();
+   
+    } catch (error) {console.log(error)}
+  }
 
   useEffect(() => {
-    async function loadPosts() {
-      try {
-        const response = await fetch("/api/posts", {
-          method: "GET",
-        });
-        console.log("I am here");
-        const posts = await response.json();
-        console.log("line 23");
-        console.log(response);
-        console.log(posts);
-        setPosts(posts);
-      } catch (error) {
-        console.log(error);
-      }
-    }
     loadPosts();
-
-    console.log(posts);
-  }, [posts]);
+    console.log(location.state.posts.username);
+  }, []);
 
   return (
     <div className="valuation-container">
       <div className="post-container ">
         <div className="post-header">
           <img className="profile-pic" src="./profile_pic.jpg" alt="somePost" />
-          <div className="handle">alfredo</div>
+          <div className="handle">{editPost.username}</div>
           <div className="timeOfPost"> 6 June</div>
-
           <div className="numberOfCoins">
             <BiCoinStack color="gold" />
-            <strong>2100</strong>
+            <strong>{editPost.grosscoins}</strong>
           </div>
-          <div className="grossCoins">Grooss</div>
-          {/* <div className="growthArrow">arrow</div> */}
+          <div className="grossCoins">Gross Coins</div>
           <div className="growthArrow">
             <HiTrendingUp color="green" />
           </div>
@@ -67,8 +100,13 @@ export default function Valuation() {
             <RiShareForwardLine />
           </div>
           <div>
-            <button className="lead-button">
-              Lead + 100
+            <button
+              className="lead-button"
+              onClick={() => {
+                handleLead()
+              }}
+            >
+              Lead + {editPost.leadcoins}
               <span className="UCoin">U</span>
             </button>
           </div>
@@ -85,13 +123,13 @@ export default function Valuation() {
             <div>
               <BiCoinStack color="gold" />
             </div>
-            <div>1100</div>
+            <div>{editPost.netcoins}</div>
             <div>
               <HiTrendingUp color="green" />
             </div>
           </div>
           <div className="bottom-username">
-            <strong>terry_dias</strong> in Lead
+            <strong>{editPost.highestLeader}</strong> in Lead
           </div>
           <div className="timer">
             <span className="timer-comp">144</span> :
